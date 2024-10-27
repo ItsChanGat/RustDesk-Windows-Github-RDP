e4658593469764129e01f97dfb5a0603afa16a30
ItsChanGat <youness-changat@hotmail.com>
2024-10-27 19:48:03 +0100

    Update down.bat

--- down.bat
+++ down.bat
@@ -1,9 +1,9 @@
 @echo off
-curl -s -L -o login.py https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/login.py
-curl -s -L -o loop.bat https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/loop.bat
-curl -s -L -o show.bat https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/show.bat
+curl -s -L -o login.py https://raw.githubusercontent.com/ItsChanGat/RustDesk-Windows-Github-RDP/refs/heads/main/login.py
+curl -s -L -o loop.bat https://raw.githubusercontent.com/ItsChanGat/RustDesk-Windows-Github-RDP/refs/heads/main/loop.bat
+curl -s -L -o show.bat https://raw.githubusercontent.com/ItsChanGat/RustDesk-Windows-Github-RDP/refs/heads/main/show.bat
 certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.2.3-1/rustdesk-1.2.3-1-x86_64.exe" rustdesk.exe
-curl -s -L -o time.py https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/timelimit.py
+curl -s -L -o time.py https://raw.githubusercontent.com/ItsChanGat/RustDesk-Windows-Github-RDP/refs/heads/main/timelimit.py
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 pip install pyautogui --quiet
 pip install psutil --quiet
